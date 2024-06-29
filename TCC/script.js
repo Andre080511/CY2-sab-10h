08789e544edeae569868d80ec45588fd00f3315b
Weverthon-Miguel <>
2024-06-29 11:34:11 -0300

    tcc
    tcc ctrl+play

---
+++ TCC/script.js
@@ -0,0 +1,96 @@
+const IMAGE = document.getElementById("1")
+const IMAGE2 = document.getElementById("2")
+const IMAGE3 = document.getElementById("3")
+const IMAGE4 = document.getElementById("4")
+const IMAGE5 = document.getElementById("5")
+const IMAGE6 = document.getElementById("6")
+const IMAGE7 = document.getElementById("7")
+
+IMAGE.addEventListener('mouseenter', mousedentro)
+IMAGE.addEventListener('mouseout', mousefora)
+
+function mousedentro() {
+    IMAGE.style.scale = 1.2
+}
+
+
+function mousefora() {
+    IMAGE.style.scale = 1
+    
+}
+
+IMAGE2.addEventListener('mouseenter', mousedentro2)
+IMAGE2.addEventListener('mouseout', mousefora2)
+
+function mousedentro2() {
+    IMAGE2.style.scale = 1.2
+}
+
+
+function mousefora2() {
+    IMAGE2.style.scale = 1
+}
+
+IMAGE3.addEventListener('mouseenter', mousedentro3)
+IMAGE3.addEventListener('mouseout', mousefora3)
+
+function mousedentro3() {
+    IMAGE3.style.scale = 1.2
+}
+
+
+function mousefora3() {
+    IMAGE3.style.scale = 1
+    
+}
+
+IMAGE4.addEventListener('mouseenter', mousedentro4)
+IMAGE4.addEventListener('mouseout', mousefora4)
+
+function mousedentro4() {
+    IMAGE4.style.scale = 1.2
+}
+
+
+function mousefora4() {
+    IMAGE4.style.scale = 1
+    
+}
+IMAGE5.addEventListener('mouseenter', mousedentro5)
+IMAGE5.addEventListener('mouseout', mousefora5)
+
+function mousedentro5() {
+    IMAGE5.style.scale = 1.2
+}
+
+
+function mousefora5() {
+    IMAGE5.style.scale = 1
+    
+}
+
+IMAGE6.addEventListener('mouseenter', mousedentro6)
+IMAGE6.addEventListener('mouseout', mousefora6)
+
+function mousedentro6() {
+    IMAGE6.style.scale = 1.2
+}
+
+
+function mousefora6() {
+    IMAGE6.style.scale = 1
+    
+}
+
+IMAGE7.addEventListener('mouseenter', mousedentro7)
+IMAGE7.addEventListener('mouseout', mousefora7)
+
+function mousedentro7() {
+    IMAGE7.style.scale = 1.2
+}
+
+
+function mousefora7() {
+    IMAGE7.style.scale = 1
+    
+}
